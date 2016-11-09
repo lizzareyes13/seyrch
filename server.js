5d14c310a3ebe0ebdcb923fd147f1af05ab56f7e
Lizza Reyes <lizzareyes13@gmail.com>
2016-11-08 22:24:12 -0500

    added overlay, result div, and results are being displayed on results div

--- server.js
+++ server.js
@@ -13,10 +13,13 @@ const YELP_TOKENSECRET = "uLHRQlxpAUadPG1O3Tidssd43Xo";
 //
 app.get("/", function(req,res){
   console.log("received request");
-  res.send("Hello World");
+  res.sendFile(__dirname + '/webpage/index.html');
   //to generate a random number, the code would be:
     // res.send("" + Math.floor(Math.random()*9));
-})
+});
+app.get('/css/style.css', function(req,res){
+  res.sendFile(__dirname + '/webpage/css/style.css');
+});
 
 app.get("/random/:start/:end", function(req,res){
   var start = req.params.start;
@@ -39,21 +42,29 @@ yelp.search({ term: 'sushi', location: 'Miami, FL' })
 })
 .catch(function (err) {
   console.error(err);
-  res.send(error);
+  res.send(err);
 });
 });
- // app.get("/laughing", function(req,res){
-//   res.send("hahahahhaha");
-// });
-//
-// app.get("/ransom", function(req, res){
-//   res.send("we have your mother.");
-// })
-//
-// app.get("/randomnumbers50and75", function(req,res){
-//   res.send("" + Math.floor(Math.random()*25+50));
-// })
 
-app.listen(3000, function() {
+
+//SEARCH REQUEST
+app.get('/seyrch/:city/:term',function(req,res){
+  var yelp = new Yelp({
+    consumer_key: YELP_KEY,
+    consumer_secret: YELP_SECRET,
+    token: YELP_TOKEN,
+    token_secret: YELP_TOKENSECRET,
+  })
+  yelp.search({ term: req.params.term, location: req.params.city})
+  .then(function(data){
+    console.log(JSON.stringify(data,null, 2));
+    res.json(data);
+  })
+  .catch(function(err){
+    console.error(err);
+    res.send(err);
+  });
+});
+app.listen(3000,function(){
   console.log("server listening on port 3000");
 });
